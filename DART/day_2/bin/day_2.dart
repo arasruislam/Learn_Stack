29916ab1cb500eccd829b955e0816d0cd0499f5b
arasruislam <arasru01@gmail.com>
2025-08-17 14:07:04 +0600

    Dart journey day 2

---
+++ DART/day_2/bin/day_2.dart
@@ -0,0 +1,21 @@
+void main(){
+  // Dart List
+  var cities = ["Dhaka", "Rangpur", "Chittagram", "Netrokona", "Barisal"];
+  // print(cities[0]);
+
+  //   Dart map
+  var student = {
+    'name': 'Asru islam',
+    'age': 23,
+    "university": "City University",
+    "program": "BSc in CSE"
+  };
+  // print(student['university']);
+
+  //   constant variable/value
+  const pi = 3.1416;
+  final age = 23;
+  print(pi);
+  print(age);
+
+}
