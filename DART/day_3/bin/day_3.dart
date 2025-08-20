910fa0348bafd2fa2a5d64f16ce440e08a5e4f06
arasruislam <arasru01@gmail.com>
2025-08-20 08:05:54 +0600

    Dart journey day 3

---
+++ DART/day_3/bin/day_3.dart
@@ -0,0 +1,16 @@
+void main(){
+  var city = ['dhaka', "gazipur", 'netrokona', 'rangpur'];
+
+  print(city.length);
+  print(city.reversed);
+  print(city.first);
+  print(city.isEmpty);
+  print(city.last);
+
+
+  var citySet = <String>{ 'dhaka', 'khulna', 'barisal'
+  };
+
+  print(citySet);
+
+}
