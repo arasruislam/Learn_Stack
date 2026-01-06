2a925987552dbc20205472a31cb70493ae87fedc
arasruislam <arasru01@gmail.com>
2026-01-06 18:01:06 +0600

    setup the color schema on tailwind.config.js

--- JAVASCRIPT/RNative/mobile_movie_app/app/index.tsx
+++ JAVASCRIPT/RNative/mobile_movie_app/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-accent">
         Welcome to Nativewind!
       </Text>
     </View>
