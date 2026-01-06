4a4a39f20778baad689d932e95ca8332a3ad4b22
arasruislam <arasru01@gmail.com>
2026-01-06 18:13:54 +0600

    nativewind setup issue: solved

--- JAVASCRIPT/RNative/mobile_movie_app/app/index.tsx
+++ JAVASCRIPT/RNative/mobile_movie_app/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex items-center justify-center bg-white">
+    <View className="flex items-center justify-center bg-white p-4">
       <Text className="text-xl font-bold text-accent">
         Welcome to Nativewind!
       </Text>
