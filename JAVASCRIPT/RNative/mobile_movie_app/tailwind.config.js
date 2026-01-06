3572f5b7e38e4cecd13407e7c29d94768f1738c7
arasruislam <arasru01@gmail.com>
2026-01-06 18:50:05 +0600

    setup routes

--- JAVASCRIPT/RNative/mobile_movie_app/tailwind.config.js
+++ JAVASCRIPT/RNative/mobile_movie_app/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}", // expo-router
+    "./app/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
